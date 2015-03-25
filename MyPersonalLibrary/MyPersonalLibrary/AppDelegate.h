0d6224de7de4aaabe7e59192602480ccd786fbe4
MrOJ <251362783@qq.com>
2015-03-25 21:12:26 +0800

    Version 0.1.0

---
+++ MyPersonalLibrary/MyPersonalLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPersonalLibrary
+//
+//  Created by Mr.OJ on 15/3/25.
+//  Copyright (c) 2015年 Mr.OJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPersonalLibrary/MyPersonalLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPersonalLibrary
+//
+//  Created by Mr.OJ on 15/3/25.
+//  Copyright (c) 2015年 Mr.OJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
